ba9d265b2382c42be787b8b7affac6ec899c7405
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-05-01 18:11:16 +0200

    add name attribute to make berkshelf happy

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "a.rosenhagen@ffuenf.de"
 license          "Apache 2.0"
 description      "installs/configures modman"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "modman"
 version          "2.0.0"
 
 %w{ debian }.each do |os|
